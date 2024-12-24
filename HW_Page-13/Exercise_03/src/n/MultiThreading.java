43c44c51e37436e8bda18827658d538a288b11b7
dmytro <dm4tro.d@gmail.com>
2024-12-24 19:13:34 +0100

    page 13 done, I will try later to do it with module info java file

--- HW_Page-13/Exercise_03/src/n/MultiThreading.java
+++ HW_Page-13/Exercise_03/src/n/MultiThreading.java
@@ -6,19 +6,19 @@ public class MultiThreading {
     public static void main(String[] args) throws InterruptedException {
         ArrayList<CounterThread> threads = new ArrayList<>();
         
-        // Threads erstellen und starten
+        
         for (int i = 0; i < 5; i++) {
-            CounterThread thread = new CounterThread(i + 1); // Nummerierung ab 1
+            CounterThread thread = new CounterThread(i + 1); 
             threads.add(thread);
             thread.start();
         }
 
-        // Warten, bis alle Threads fertig sind
+        
         for (int i = 0; i < 5; i++) {
             threads.get(i).join();
         }
 
-        // Abschlussmeldung
+      
         System.out.println("Fertig");
     }
 }
